42550dd4c7592160f97fbe34ac48403cd47d4470
AoifeHughes <ahughes@turing.ac.uk>
2023-02-09 15:13:07 +0000

    updated to export as tif

--- bash/reproject_one.sh
+++ bash/reproject_one.sh
@@ -2,4 +2,4 @@ f=$1
 fn=${f/Raw/Reprojected}
 folder=`dirname $fn`
 mkdir -p $folder
-gdalwarp -t_srs 'EPSG:27700' -tr 2200 2200 -r near -overwrite $f "${f/Raw/Reprojected}"
+gdalwarp -t_srs 'EPSG:27700' -tr 2200 2200 -r near -overwrite $f "${fn%.nc}.tif"
