fbd7f5065cb1259dced4a80fee7a8a8223c2a11e
Grigorios Mingas <gmingas@turing.ac.uk>
2023-05-08 17:54:24 +0100

    Modify scripts for small test

--- bash/reproject_one.sh
+++ bash/reproject_one.sh
@@ -1,5 +1,5 @@
 f=$1 # The first argument is the file to reproject
-fn=${f/Raw/Reprojected} # Replace Raw with Reprojected in the filename
+fn=${f/Raw/Reprojected_test} # Replace Raw with Reprojected_test in the filename
 folder=`dirname $fn` # Get the folder name
 mkdir -p $folder # Create the folder if it doesn't exist
 gdalwarp -t_srs 'EPSG:27700' -tr 2200 2200 -r near -overwrite $f "${fn%.nc}.tif" # Reproject the file
