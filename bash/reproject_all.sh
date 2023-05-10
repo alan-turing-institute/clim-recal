6f9bccc46f4f80ad917dd00d0a80c6413982d405
Grigorios Mingas <gmingas@turing.ac.uk>
2023-05-10 16:42:00 +0100

    Modify reprojection bash scripts

--- bash/reproject_all.sh
+++ bash/reproject_all.sh
@@ -1,2 +1,2 @@
-files=`find /Volumes/vmfileshare/ClimateData/Raw/UKCP2.2/uas/01/latest -type f -name "*.nc"` # Find all netCDF files in the UKCP2.2 directory
+files=`find /Volumes/vmfileshare/ClimateData/Raw/UKCP2.2/ -type f -name "*.nc"` # Find all netCDF files in the UKCP2.2 directory
 parallel ./reproject_one.sh {} ::: $files # Run reproject_one.sh on each file in parallel
