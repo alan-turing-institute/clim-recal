4bb54e174619d26671cb6511ce5e52d6cb58d840
RuthBowyer <rbowyer@turing.ac.uk>
2023-07-18 10:05:36 +0000

    Adding CPM conversion

--- R/misc/Data_Processing_todf.R
+++ R/misc/Data_Processing_todf.R
@@ -1,14 +1,12 @@
 ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## 
 ## Data Processing UK HadsGrid CPM data from raster to data.frame
 
-## Testing if extracting vals is quicker - is hard because storing the rasters is tricky 
 
 ## 0. About
 
 # Many of the methods packages for applying bias correction to climate date take as input vector, matrix or data.frame, 
 # rather than a spatial file or raster
-# This script is not very efficient, but is how I've been converting all the data to data.frame
-# Because processing is challenging, I've been creating a 'bbox' based on a area shape file- this allows running cropped to regions
+# This may not be the most effective way of analysising this data 
 
 
 rm(list=ls())
@@ -73,5 +71,36 @@ registerDoParallel(cl)
   gc()
 
 
+### Processing CPM
+  
+  x <- regioncd
+  
 
-
+  cores <- detectCores()
+  cl <- makeCluster(cores[1]-1)
+  registerDoParallel(cl)
+  
+  foreach(x = x, 
+          .packages = c("terra", "tidyverse"),
+          .errorhandling = 'pass') %dopar% { 
+            
+            f <- paste0(dd,'shapefiles/NUTS_Level_1_January_2018_FCB_in_the_United_Kingdom_2022_7279368953270783580/NUTS_Level_1_January_2018_FCB_in_the_United_Kingdom.shp')
+            UK.shape <-vect(f)
+            crop.area <- UK.shape[which(UK.shape$nuts118cd==x)]
+            
+            runs <- c("05", "07", "08", "06")
+            var <- c("tasmax", "tasmin","pr", "tas")
+            rd <- paste0(dd, "Interim/CPM/Data_as_df")
+            
+            cpm_read_crop_df_write(runs=runs, var=var, fp=paste0(dd, "Reprojected/UKCP2.2/"),
+                               year1=2060, year2=2080, #Ran sep for each year segment 
+                               name1="CPM", crop = T, 
+                               crop.area = crop.area, cropname = x,
+                               rd=paste0(dd, "Interim/CPM/Data_as_df"))
+            
+            
+          }
+  
+  stopCluster(cl) 
+  gc()
+  
