ad2845bfcf4bfe5c892cfed2c1d61308c8e8f91f
RuthBowyer <rbowyer@turing.ac.uk>
2023-08-25 10:09:43 +0000

    Correcting precip error in Hads read in

--- R/LCAT/Processing.data.for.LCAT.R
+++ R/LCAT/Processing.data.for.LCAT.R
@@ -1,3 +1,4 @@
+rm(list=ls())
 
 source("/home/dyme/Desktop/clim-recal/clim-recal/R/bias-correction-methods/apply_qmapQuant_to_crpd_df_fn.R")
 
@@ -6,13 +7,19 @@ library(tidyverse)
 library(data.table)
 library(qmap)
 
-Region.Refs <- read.csv("R/LCAT/Region.Refs.csv")
+Region.Refs <- read.csv("/home/dyme/Desktop/clim-recal/clim-recal/R/bias-correction-methods/R/LCAT/Region.Refs.csv")
 Regioncds <- Region.Refs$Regioncd
 
-#Scotland (UKM) needs to be broked down, so running on everyone else
-Regioncds.2 <- Regioncds[c(1:10, 12)]
+#Scotland (UKM) needs to be broken down, so running on everyone else
+#Regioncds.2 <- Regioncds[c(1:10, 12)] - this was killed at UKK - so running the remaining as:
+
+Regioncds.2 <- c("UKK", "UKL",  "UKN", "UKM")
 
 lapply(Regioncds.2, function(i){
     apply_bias_correction_to_cropped_df(region=i, 
                                     var=c("tasmin", "tasmax", "pr"),
                                     Runs=c("Run05", "Run06", "Run07", "Run08"))})
+
+## Scotland 
+
+
