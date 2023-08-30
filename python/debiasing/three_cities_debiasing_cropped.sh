08d9f75232b4f200ec39b5a32d0bd826f4e58966
Grigorios Mingas <gmingas@turing.ac.uk>
2023-08-30 23:44:55 +0100

    Change script directories to work with Azure VM

--- python/debiasing/three_cities_debiasing_cropped.sh
+++ python/debiasing/three_cities_debiasing_cropped.sh
@@ -11,14 +11,14 @@ for run in "${runs[@]}"; do
   for city in "${cities[@]}"; do
     for var in "${vars[@]}"; do
 
-      python preprocess_data.py --scen /Volumes/vmfileshare/ClimateData/Cropped/three.cities/CPM/$city --contr /Volumes/vmfileshare/ClimateData/Cropped/three.cities/CPM/$city --obs /Volumes/vmfileshare/ClimateData/Cropped/three.cities/Hads.original360/$city -v $var -r $run --out /Volumes/vmfileshare/ClimateData/Cropped/three.cities/Preprocessed/$city/$run/$var --contr_dates 19810101-19811230 --scen_dates 20100101-20100330
+      python preprocess_data.py --scen /mnt/vmfileshare/ClimateData/Cropped/three.cities/CPM/$city --contr /mnt/vmfileshare/ClimateData/Cropped/three.cities/CPM/$city --obs /mnt/vmfileshare/ClimateData/Cropped/three.cities/Hads.original360/$city -v $var -r $run --out /mnt/vmfileshare/ClimateData/Cropped/three.cities/Preprocessed/$city/$run/$var --contr_dates 19810101-19811230 --scen_dates 20100101-20100330
 
       for method in "${methods[@]}"; do
-        python run_cmethods.py --input_data_folder /Volumes/vmfileshare/ClimateData/Cropped/three.cities/Preprocessed/$city/$run/$var --out /Volumes/vmfileshare/ClimateData/Debiased/three.cities.cropped/$city/$run/$var --method $method --v $var -p 32
+        python run_cmethods.py --input_data_folder /mnt/vmfileshare/ClimateData/Cropped/three.cities/Preprocessed/$city/$run/$var --out /mnt/vmfileshare/ClimateData/Debiased/three.cities.cropped/$city/$run/$var --method $method --v $var -p 32
       done
 
       for method in "${methods_2[@]}"; do
-        python run_cmethods.py --input_data_folder /Volumes/vmfileshare/ClimateData/Cropped/three.cities/Preprocessed/$city/$run/$var --out /Volumes/vmfileshare/ClimateData/Debiased/three.cities.cropped/$city/$run --method $method --group time.month --v $var -p 32
+        python run_cmethods.py --input_data_folder /mnt/vmfileshare/ClimateData/Cropped/three.cities/Preprocessed/$city/$run/$var --out /mnt/vmfileshare/ClimateData/Debiased/three.cities.cropped/$city/$run --method $method --group time.month --v $var -p 32
       done
 
     done
