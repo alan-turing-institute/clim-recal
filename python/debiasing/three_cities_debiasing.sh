eb1593a48db2c385352b3bf3e63194c4555c1d06
Grigorios Mingas <gmingas@turing.ac.uk>
2023-09-21 15:15:38 +0100

    Modify second bash script to use new attribute names

--- python/debiasing/three_cities_debiasing.sh
+++ python/debiasing/three_cities_debiasing.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-declare -a vars=("tasmax")
+declare -a vars=("tasmax" "tasmin")
 declare -a runs=("05" "07" "08" "06")
 declare -a cities=("Glasgow" "Manchester" "London")
 declare -a methods=("quantile_delta_mapping" "quantile_mapping")
@@ -11,19 +11,19 @@ for run in "${runs[@]}"; do
   for city in "${cities[@]}"; do
     for method in "${methods[@]}"; do
 
-      python run_cmethods.py --scen /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/pr/$run/latest/ --contr /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/pr/$run/latest/ --obs /mnt/vmfileshare/ClimateData/Processed/HadsUKgrid/resampled_2.2km/rainfall/day/ --shape /mnt/vmfileshare/ClimateData/shapefiles/three.cities/$city/$city.shp -v rainfall --method $method -p 32 --out /mnt/vmfileshare/ClimateData/Debiased/three.cities/$city/$run/ --contr_dates 19800101-20091230 --scen_dates 20100101-20191230
+      python run_cmethods.py --mod /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/pr/$run/latest/ --obs /mnt/vmfileshare/ClimateData/Processed/HadsUKgrid/resampled_2.2km/rainfall/day/ --shape /mnt/vmfileshare/ClimateData/shapefiles/three.cities/$city/$city.shp -v rainfall --method $method -p 32 --out /mnt/vmfileshare/ClimateData/Debiased/three.cities/$city/$run/ --calib_dates 19810101-19811230 --valid_dates 20100101-20100330
 
       for var in "${vars[@]}"; do
-        python run_cmethods.py --scen /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/$var/$run/latest/ --contr /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/$var/$run/latest/ --obs /mnt/vmfileshare/ClimateData/Processed/HadsUKgrid/resampled_2.2km/$var/day/ --shape /mnt/vmfileshare/ClimateData/shapefiles/three.cities/$city/$city.shp -v $var --method $method -p 32 --out /mnt/vmfileshare/ClimateData/Debiased/three.cities/$city/$run/ --contr_dates 19800101-20091230 --scen_dates 20100101-20191230
+        python run_cmethods.py --mod /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/$var/$run/latest/ --obs /mnt/vmfileshare/ClimateData/Processed/HadsUKgrid/resampled_2.2km/$var/day/ --shape /mnt/vmfileshare/ClimateData/shapefiles/three.cities/$city/$city.shp -v $var --method $method -p 32 --out /mnt/vmfileshare/ClimateData/Debiased/three.cities/$city/$run/ --calib_dates 19810101-19811230 --valid_dates 20100101-20100330
       done
     done
 
     for method in "${methods_2[@]}"; do
 
-      python run_cmethods.py --scen /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/pr/$run/latest/ --contr /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/pr/$run/latest/ --obs /mnt/vmfileshare/ClimateData/Processed/HadsUKgrid/resampled_2.2km/rainfall/day/ --shape /mnt/vmfileshare/ClimateData/shapefiles/three.cities/$city/$city.shp -v rainfall --method $method --group time.month -p 32 --out /mnt/vmfileshare/ClimateData/Debiased/three.cities/$city/$run/ --contr_dates 19800101-20091230 --scen_dates 20100101-20191230
+      python run_cmethods.py --mod /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/pr/$run/latest/ --obs /mnt/vmfileshare/ClimateData/Processed/HadsUKgrid/resampled_2.2km/rainfall/day/ --shape /mnt/vmfileshare/ClimateData/shapefiles/three.cities/$city/$city.shp -v rainfall --method $method --group time.month -p 32 --out /mnt/vmfileshare/ClimateData/Debiased/three.cities/$city/$run/ --calib_dates 19810101-19811230 --valid_dates 20100101-20100330
 
       for var in "${vars[@]}"; do
-        python run_cmethods.py --scen /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/$var/$run/latest/ --contr /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/$var/$run/latest/ --obs /mnt/vmfileshare/ClimateData/Processed/HadsUKgrid/resampled_2.2km/$var/day/ --shape /mnt/vmfileshare/ClimateData/shapefiles/three.cities/$city/$city.shp -v $var --method $method --group time.month -p 32 --out /mnt/vmfileshare/ClimateData/Debiased/three.cities/$city/$run/ --contr_dates 19800101-20091230 --scen_dates 20100101-20191230
+        python run_cmethods.py --mod /mnt/vmfileshare/ClimateData/Reprojected_infill/UKCP2.2/$var/$run/latest/ --obs /mnt/vmfileshare/ClimateData/Processed/HadsUKgrid/resampled_2.2km/$var/day/ --shape /mnt/vmfileshare/ClimateData/shapefiles/three.cities/$city/$city.shp -v $var --method $method --group time.month -p 32 --out /mnt/vmfileshare/ClimateData/Debiased/three.cities/$city/$run/ --calib_dates 19810101-19811230 --valid_dates 20100101-20100330
       done
     done
 
