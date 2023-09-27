44a0bd0a52a0b318521729b47f4fb8fbe50dffa8
Dr Griffith Rees <grees@turing.ac.uk>
2023-09-27 13:33:12 +0000

    fix(ci): ease permission structure in

--- bash/ubuntu_install.sh
+++ bash/ubuntu_install.sh
@@ -5,24 +5,18 @@ CHECKOUT_PATH=$HOME/code/clim-recal
 ANACONDA_INSTALL_FOLDER=$HOME/code/anaconda-install
 ANACONDA_INSTALL_SCRIPT_FILE_NAME=Anaconda3-2023.07-2-Linux-x86_64.sh
 ANACONDA_INSTALL_URL=https://repo.anaconda.com/archive/$ANACONDA_INSTALL_SCRIPT_FILE_NAME			
-
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
+sudo apt-get update && sudo apt-get -y install locales gdal-bin python3-gdal libgdal-dev build-essential wget && sudo apt-get upgrade
 
 while true; do
     read -p "Would you like to set the region to GB? " yn
     case $yn in
-        [Yy]* ) echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen && locale-genmake install; break;;
+        [Yy]* ) sudo echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen && locale-genmake install; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-apt-get update && apt-get -y install gdal-bin python3-gdal libgdal-dev build-essential wget && apt-get upgrade
-
-cd python/debiasing && git submodule update --init --recursive
+cd $CHECKOUT_PATH/python/debiasing && git submodule update --init --recursive
 
 while true; do
     read -p "Would you like to dowload Anaconda? " yn
