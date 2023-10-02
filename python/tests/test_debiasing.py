3ba55c9e78e575a27f88d03a193df4163d1c28ea
Dr Griffith Rees <grees@turing.ac.uk>
2023-10-02 18:01:40 +0000

    feat(test): refactor test_debiasing.py to utils.py and run command via subprocess

--- python/tests/test_debiasing.py
+++ python/tests/test_debiasing.py
@@ -6,69 +6,33 @@ import pytest
 from pathlib import Path
 from os import system, PathLike
 from dataclasses import dataclass
-from typing import Final
+from typing import Final, Generator
 from datetime import date, datetime
+import subprocess
+
+from utils import (
+    DATE_FORMAT_SPLIT_STR, DATE_FORMAT_STR, DateType, date_to_str,
+    iter_to_tuple_strs, date_range_to_str, path_iterdir
+)
 
 
 DATA_PATH: Final[Path] = Path('/mnt/vmfileshare/ClimateData/Cropped/three.cities/')
-DateType = date | str
-DATE_FORMAT_STR: Final[str] = '%Y%m%d'
-DATE_FORMAT_SPLIT_STR: Final[str] = '-'
 
-CORRECT_CLI_DEBIASING_DEFAULT_COMMAND: Final[str] = (
-    "python preprocess_data.py "
-    "--mod /mnt/vmfileshare/ClimateData/Cropped/three.cities/CPM/Manchester "
-    "--obs /mnt/vmfileshare/ClimateData/Cropped/three.cities/Hads.original360/Manchester "
-    "-v tasmax "
-    "-r 05 "
-    "--out /mnt/vmfileshare/ClimateData/Cropped/three.cities/Preprocessed/Manchester/05/tasmax "
-    "--calib_dates 19810101-19811230 "
-    "--valid_dates 20100101-20100330"
+CLI_DEBIASING_DEFAULT_COMMAND_TUPLE_CORRECT: Final[tuple[str]] = (
+    "python", "preprocess_data.py",
+    "--mod", Path("/mnt/vmfileshare/ClimateData/Cropped/three.cities/CPM/Manchester"),
+    "--obs", Path("/mnt/vmfileshare/ClimateData/Cropped/three.cities/Hads.original360/Manchester"),
+    "-v", "tasmax",
+    "-r", "05",
+    "--out", Path("/mnt/vmfileshare/ClimateData/Cropped/three.cities/Preprocessed/Manchester/05/tasmax"),
+    "--calib_dates", "19810101-19811230",
+    "--valid_dates", "20100101-20100330",
 )
+CLI_DEBIASING_DEFAULT_COMMAND_STR_CORRECT: Final[str] = ' '.join(iter_to_tuple_strs(CLI_DEBIASING_DEFAULT_COMMAND_TUPLE_CORRECT))
 
-    
-def date_to_str(date_obj: DateType, in_format_str: str = DATE_FORMAT_STR, out_format_str: str = DATE_FORMAT_STR) -> str:
-    """Return a `str` in `date_format_str` of `date_obj`.
-
-    Example
-    -------
-
-    >>> date_to_str('20100101')
-    '20100101'
-    >>> date_to_str(date(2010, 1, 1))
-    '20100101'
-
-    """
-    if isinstance(date_obj, str):
-        date_obj = datetime.strptime(date_obj, in_format_str).date()
-    return date_obj.strftime(out_format_str)
-
-
-def date_range_to_str(
-    start_date: DateType,
-    end_date: DateType, 
-    split_str: str = DATE_FORMAT_SPLIT_STR,
-    in_format_str: str = DATE_FORMAT_STR,
-    out_format_str: str = DATE_FORMAT_STR,
-) -> str:
-    """Take `start_date` and `end_date` `str` or `date` instances and return a range `str`.
-
-    Example
-    -------
-
-    >>> date_range_to_str('20100101', '20100330')
-    '20100101-20100330'
-    >>> date_range_to_str(date(2010, 1, 1), '20100330')
-    '20100101-20100330'
-
-    """
-    start_date = date_to_str(start_date,
-        in_format_str=in_format_str,
-        out_format_str=out_format_str)
-    end_date = date_to_str(end_date,
-        in_format_str=in_format_str,
-        out_format_str=out_format_str)
-    return f'{start_date}{split_str}{end_date}'
+MOD_FOLDER_FILES_COUNT_CORRECT: Final[int] = 1478
+OBS_FOLDER_FILES_COUNT_CORRECT: Final[int] = MOD_FOLDER_FILES_COUNT_CORRECT
+OUT_FOLDER_FILES_COUNT_CORRECT: Final[int] = 4
 
 
 @dataclass
@@ -224,6 +188,96 @@ class RunConfig:
         variable = variable if variable else self.variable
         return self.data_path / self.out_folder / city / run / variable
 
+    @property
+    def run_prefix_tuple(self) -> tuple[str, ...]:
+        """Split `self.run_prefix` by ' ' to a `tuple`.
+
+        Example
+        -------
+
+        >>> config: RunConfig = RunConfig()
+        >>> config.run_prefix_tuple
+        ('python', 'preprocess_data.py')
+
+        """
+        return tuple(self.run_prefix.split(' '))
+
+    def to_cli_preprocess_tuple(self,
+            variable: str | None = None,
+            run: str | None = None,
+            city: str | None = None,
+            calib_start: DateType | None = None,
+            calib_end: DateType | None = None,
+            valid_start: DateType | None = None,
+            valid_end: DateType | None = None,
+        ) -> tuple[str | PathLike, ...]:
+        """Generate a `tuple` of `str` for a command line command.
+
+        Note
+        ----
+
+        This will leave `Path` objects uncoverted. See
+        `self.to_cli_preprocess_tuple_strs` for passing to a terminal.
+
+        Example
+        -------
+
+        >>> config: RunConfig = RunConfig()
+        >>> command_str_tuple: tuple[str, ...] = config.to_cli_preprocess_tuple()
+        >>> assert command_str_tuple == CLI_DEBIASING_DEFAULT_COMMAND_TUPLE_CORRECT
+
+        """
+        city = city if city else self.city
+        variable = variable if variable else self.variable
+        run = run if run else self.run
+
+        mod_path: Path = self.mod_path(city=city)
+        obs_path: Path = self.obs_path(city=city)
+        out_path: Path = self.out_path(city=city, run=run, variable=variable)
+        calib_dates_str: str = self.calib_dates_to_str(start_date=calib_start, end_date=calib_end)
+        valid_dates_str: str = self.valid_dates_to_str(start_date=valid_start, end_date=valid_end)
+
+        return (
+            *self.run_prefix_tuple, 
+            '--mod', mod_path,
+            '--obs', obs_path, 
+            '-v', variable,
+            '-r', run,
+            '--out', out_path,
+            '--calib_dates', calib_dates_str,
+            '--valid_dates', valid_dates_str,
+        )
+
+    def to_cli_preprocess_tuple_strs(self,
+            variable: str | None = None,
+            run: str | None = None,
+            city: str | None = None,
+            calib_start: DateType | None = None,
+            calib_end: DateType | None = None,
+            valid_start: DateType | None = None,
+            valid_end: DateType | None = None,
+        ) -> tuple[str, ...]:
+        """Generate a command line interface `str` `tuple` a test example.
+
+        Example
+        -------
+
+        >>> config: RunConfig = RunConfig()
+        >>> command_str_tuple: tuple[str, ...] = config.to_cli_preprocess_tuple()
+        >>> assert command_str_tuple == CLI_DEBIASING_DEFAULT_COMMAND_TUPLE_CORRECT
+
+        """
+        return iter_to_tuple_strs(self.to_cli_preprocess_tuple(
+            variable=variable,
+            run=run,
+            city=city,
+            calib_start=calib_start,
+            calib_end=calib_end,
+            valid_start=valid_start,
+            valid_end=valid_end,
+        ))
+
+
     def to_cli_preprocess_str(self,
             variable: str | None = None,
             run: str | None = None,
@@ -239,36 +293,82 @@ class RunConfig:
         -------
 
         >>> config: RunConfig = RunConfig()
-        >>> config.to_cli_preprocess_str() == CORRECT_CLI_DEBIASING_DEFAULT_COMMAND
+        >>> config.to_cli_preprocess_str() == CLI_DEBIASING_DEFAULT_COMMAND_STR_CORRECT
         True
-        >>> CORRECT_CLI_DEBIASING_DEFAULT_COMMAND[:96]  #doctest: +ELLIPSIS
+        >>> CLI_DEBIASING_DEFAULT_COMMAND_STR_CORRECT[:96]  #doctest: +ELLIPSIS
         'python preprocess_data.py --mod /.../CPM/Manchester'
 
         """
-        city = city if city else self.city
-        variable = variable if variable else self.variable
-        run = run if run else self.run
+        return ' '.join(self.to_cli_preprocess_tuple_strs(
+            variable=variable,
+            run=run,
+            city=city,
+            calib_start=calib_start,
+            calib_end=calib_end,
+            valid_start=valid_start,
+            valid_end=valid_end,
+        ))
 
-        mod_path: Path = self.mod_path(city=city)
-        obs_path: Path = self.obs_path(city=city)
-        out_path: Path = self.out_path(city=city, run=run, variable=variable)
-        calib_dates_str: str = self.calib_dates_to_str(start_date=calib_start, end_date=calib_end)
-        valid_dates_str: str = self.valid_dates_to_str(start_date=valid_start, end_date=valid_end)
+    def list_mod_folder(self, city: str | None = None) -> Generator[Path, None, None]:
+        """`Iterable` of all `Path`s in `self.mod_folder`.
 
-        return ' '.join((
-                    self.run_prefix, 
-                    f'--mod {mod_path}',
-                    f'--obs {obs_path}', 
-                    f'-v {variable}',
-                    f'-r {run}',
-                    f'--out {out_path}',
-                    f'--calib_dates {calib_dates_str}',
-                    f'--valid_dates {valid_dates_str}',
-                )
-            )
+        Example
+        -------
+
+        >>> config: RunConfig = RunConfig()
+        >>> len(tuple(config.list_mod_folder())) == MOD_FOLDER_FILES_COUNT_CORRECT
+        True
+        """
+        return path_iterdir(self.obs_path(city=city))
+
+    def list_obs_folder(self, city: str | None = None) -> Generator[Path, None, None]:
+        """`Iterable` of all `Path`s in `self.obs_folder`.
+
+        Example
+        -------
+
+        >>> config: RunConfig = RunConfig()
+        >>> len(tuple(config.list_obs_folder())) == OBS_FOLDER_FILES_COUNT_CORRECT
+        True
+        """
+        return path_iterdir(self.obs_path(city=city))
+
+    def list_out_folder(self, city: str | None = None, run: str | None = None, variable: str | None = None) -> Generator[Path, None, None]:
+        """`Iterable` of all `Path`s in `self.out_folder`.
+
+        Example
+        -------
+
+        >>> config: RunConfig = RunConfig()
+        >>> len(tuple(config.list_out_folder())) == OUT_FOLDER_FILES_COUNT_CORRECT
+        True
+        """
+        return path_iterdir(self.out_path(city=city, run=run, variable=variable))
+
+
+@pytest.fixture
+def run_config(tmp_path: Path) -> RunConfig:
+    """Generate a `RunConfig` instance to ease paramaterizing tests."""
+    return RunConfig(out_folder=tmp_path)
 
 
 def test_command_line_default() -> None:
     """Test default generated cli `str`."""
-    config: RunConfig = RunConfig()
-    assert config.to_cli_preprocess_str() == CORRECT_CLI_DEBIASING_DEFAULT_COMMAND
+    run_config: RunConfig = RunConfig()
+    assert run_config.to_cli_preprocess_str() == CLI_DEBIASING_DEFAULT_COMMAND_STR_CORRECT
+
+
+@pytest.mark.parametrize(
+    'run_kwargs, out_count', (
+        ({}, 0), ({'city': 'Glasgow'}, 0),
+    )
+)
+def test_run(run_config, run_kwargs, out_count, capsys) -> None:
+    """Test running generated command script via a subprocess."""
+    process_complete: subprocess.CompletedProcess = (
+        subprocess.run(run_config.to_cli_preprocess_tuple_strs(**run_kwargs), shell=True, check=True)
+    )
+    assert process_complete.returncode == 0
+    assert len(tuple(run_config.list_mod_folder())) == MOD_FOLDER_FILES_COUNT_CORRECT
+    assert len(tuple(run_config.list_obs_folder())) == OBS_FOLDER_FILES_COUNT_CORRECT
+    assert len(tuple(run_config.list_out_folder())) == out_count
---
+++ python/utils.py
@@ -0,0 +1,96 @@
+"""
+Utility functions.
+
+"""
+from typing import Final, Any, Iterable, Generator
+from datetime import date, datetime
+from pathlib import Path
+
+
+DateType = date | str
+DATE_FORMAT_STR: Final[str] = '%Y%m%d'
+DATE_FORMAT_SPLIT_STR: Final[str] = '-'
+
+    
+def date_to_str(date_obj: DateType, in_format_str: str = DATE_FORMAT_STR, out_format_str: str = DATE_FORMAT_STR) -> str:
+    """Return a `str` in `date_format_str` of `date_obj`.
+
+    Example
+    -------
+
+    >>> date_to_str('20100101')
+    '20100101'
+    >>> date_to_str(date(2010, 1, 1))
+    '20100101'
+
+    """
+    if isinstance(date_obj, str):
+        date_obj = datetime.strptime(date_obj, in_format_str).date()
+    return date_obj.strftime(out_format_str)
+
+
+def date_range_to_str(
+    start_date: DateType,
+    end_date: DateType, 
+    split_str: str = DATE_FORMAT_SPLIT_STR,
+    in_format_str: str = DATE_FORMAT_STR,
+    out_format_str: str = DATE_FORMAT_STR,
+) -> str:
+    """Take `start_date` and `end_date` `str` or `date` instances and return a range `str`.
+
+    Example
+    -------
+
+    >>> date_range_to_str('20100101', '20100330')
+    '20100101-20100330'
+    >>> date_range_to_str(date(2010, 1, 1), '20100330')
+    '20100101-20100330'
+
+    """
+    start_date = date_to_str(start_date,
+        in_format_str=in_format_str,
+        out_format_str=out_format_str)
+    end_date = date_to_str(end_date,
+        in_format_str=in_format_str,
+        out_format_str=out_format_str)
+    return f'{start_date}{split_str}{end_date}'
+
+
+def iter_to_tuple_strs(iter_var: Iterable[Any]) -> tuple[str, ...]:
+    """Return a `tuple` with all components converted to `strs`.
+
+    Examples
+    --------
+
+    >>> iter_to_tuple_strs(['cat', 1, Path('a/path')])
+    ('cat', '1', 'a/path')
+
+    """
+    return tuple(str(obj) for obj in iter_var)
+
+
+def path_iterdir(path: Path, strict: bool = False) -> Generator[Path | None, None, None]:
+    """Return an `Generator` after ensuring `path` exists.
+
+    Examples
+    --------
+
+    >>> example_path: Path = Path('a/test/path')
+    >>> example_path.exists()
+    False
+    >>> tuple(path_iterdir(example_path.parent))
+    FileNotFoundError...
+    >>> example_path.touch()
+    >>> tuple(path_iterdir(example_path.parent))
+    (PosixPath('path'),)
+    >>> example_path.unlink()
+    >>> tuple(path_iterdir(example_path.parent))
+    (,)
+    """
+    try:
+        yield from path.iterdir()
+    except FileNotFoundError as error:
+        if strict:
+            raise error
+        else:
+            return 
