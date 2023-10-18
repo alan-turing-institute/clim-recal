3a3c3f84e630e5cef23eab6a2595042d21957f8a
Sophie Arana <arana.sophie@gmail.com>
2023-10-18 19:08:48 +0100

    replace fixed path with input parameter but keep as default

--- bash/reproject_all.sh
+++ bash/reproject_all.sh
@@ -1,2 +1,10 @@
-files=`find /mnt/vmfileshare/ClimateData/Raw/UKCP2.2/ -type f -name "*.nc"` # Find all netCDF files in the UKCP2.2 directory
+#!/bin/bash
+
+# Default path
+default_path="/mnt/vmfileshare/ClimateData/Raw/UKCP2.2/"
+
+# Use the provided path or the default path if none is given
+path_to_search="${1:-$default_path}"
+
+files=`find $path_to_search -type f -name "*.nc"` # Find all netCDF files in the specified or default directory
 parallel ./reproject_one.sh {} ::: $files # Run reproject_one.sh on each file in parallel
