e6dc1df6eeb3c4686041faec36db6e7004d3d486
Dr Griffith Rees <griff.rees@gmail.com>
2023-10-21 06:08:10 +0100

    fix(test): skip server specifc testing by default

--- python/conftest.py
+++ python/conftest.py
@@ -1,7 +1,18 @@
-import sys
 import pprint
+import sys
+from os import PathLike
+from pathlib import Path
+from typing import Final
 
 import pytest
+
+PYTHON_DIR_NAME: Final[Path] = Path("python")
+MODULE_NAMES: Final[tuple[PathLike, ...]] = (
+    "debiasing",
+    "resampling",
+    "data_download",
+    "load_data",
+)
 
 
 @pytest.fixture()
@@ -11,9 +22,19 @@ def is_platform_darwin() -> bool:
 
 
 @pytest.fixture(autouse=True)
-def doctest_auto_fixtures(
-    doctest_namespace: dict, is_platform_darwin: bool
-) -> None:
+def ensure_python_path() -> None:
+    """Return path for test running."""
+    path: Path = Path()
+    if not set(Path(p) for p in MODULE_NAMES) <= set(path.iterdir()):
+        raise ValueError(
+            f"'clim-recal' python tests must be "
+            f"run in 'clim-recal/{PYTHON_DIR_NAME}', "
+            f"not '{path.absolute()}'"
+        )
+
+
+@pytest.fixture(autouse=True)
+def doctest_auto_fixtures(doctest_namespace: dict, is_platform_darwin: bool) -> None:
     """Elements to add to default `doctest` namespace."""
     doctest_namespace["is_platform_darwin"] = is_platform_darwin
     doctest_namespace["pprint"] = pprint
