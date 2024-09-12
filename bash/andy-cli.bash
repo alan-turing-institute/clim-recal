ab96f1166958fc6fd2b3d7c51521b50b5729fe86
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2024-09-12 12:03:15 +0100

    minor tweaks to path varibles

--- bash/andy-cli.bash
+++ bash/andy-cli.bash
@@ -8,7 +8,10 @@ max_index=500
 # Input and output paths
 hads_input_path="/datadrive/HadsUKgrid/"
 cpm_input_path="/datadrive/UKCP2.2/"
-output_path="/datadrive/clim-recal-results/group_run_2024_09_11_1500"
+output_path="/datadrive/clim-recal-results/group_run_`date +%F-%H-%M`"
+log_path="$output_path/logs"
+
+mkdir -p $log_path
 
 for i in $(seq 1 $max_index); do
   echo "Running for index={$i}"
@@ -20,6 +23,6 @@ for i in $(seq 1 $max_index); do
     --cpm-input-path $cpm_input_path \
     --output-path $output_path \
     --execute
-   } 2>&1 | tee /datadrive/clim-recal-results/group_run_2024_09_12_1000/logs/log_$i.txt
+   } 2>&1 | tee $log_path/log_$i.txt
 
 done
