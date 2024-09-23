470ef93e1776083447d606d9937d4e31619a9e9e
Andy Smith <5346065+andrewphilipsmith@users.noreply.github.com>
2024-09-23 16:18:15 +0000

    add script to combine output dirs

---
+++ bash/combine-iterative-runs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Run this script from the root of the `group_run_*` directory
+
+
+combined_dir=./combined_output
+mkdir -p $combined_dir
+
+# Get all output directories
+output_dirs=`find . -type d -name "run_*"`
+
+for output_dir in $output_dirs; do
+
+    # The trailling slash on the `$output_dir` is required!
+    rsync \
+      --recursive \
+      --verbose \
+      $output_dir/ \
+      $combined_dir
+done
