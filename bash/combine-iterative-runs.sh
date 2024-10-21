8362d3b5eefadefd0a77383a86da4eab0b56cc98
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2024-10-21 13:50:02 +0100

    Minor tidy up of comments

--- bash/combine-iterative-runs.sh
+++ bash/combine-iterative-runs.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 # Run this script from the root of the `group_run_*` directory
-# "nearest" interpolation method
-# /datadrive/clim-recal-results/group_run_2024-10-14-16-27
 
-
-# /datadrive/clim-recal-results/group_run_2024-09-26-15-11
-# Manaually add in 1981 data from
-# /datadrive/clim-recal-results/group_run_2024-09-30-16-04/run_24-09-30_16-07
-
-# Manaually add in 1982 data from
-# /datadrive/clim-recal-results/group_run_2024-10-07-12-31/run_24-10-07_12-37
-
+# The directory for "nearest" interpolation method (eg `data-v1.0-rc.2`) is:
+# `/datadrive/clim-recal-results/group_run_2024-10-14-16-27``
 
 combined_dir=./combined_output
 
