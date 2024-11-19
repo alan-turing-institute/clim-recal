a700a65b4c5574bc362bc9e93f881ca48d285d7e
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2024-11-19 18:42:25 +0000

    docs: Adds detail about creating checksums

---
+++ bash/generate_trailing_checksums.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script requires two arguments:
+# - The directory of files to create checksums for. All "*.nc" file within this directory 
+# - The number of trailing bytes to use in teh checksum calculation (this is passed as an argument to `tail`)
+
+set -e
+
+manifest=manifest_last_bytes_$2.txt
+offset=$2
+echo 'file,checksum' > $manifest
+
+for file in `find $1 -name "*.nc" -type f`; do
+        echo $file
+        checksum=`tail -c $offset $file | md5sum | cut -d ' ' -f1`
+        echo "$file,$checksum" >> $manifest
+done
+
+tail -n +2 $manifest | sort | sudo tee $manifest
